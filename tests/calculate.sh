133ece7ada948bad409a6b88ffc3610e69af741a
Gavin Peng <gpeng@oicr.on.ca>
2022-06-19 14:18:27 -0400

    make adjustment to test commands

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 cd $1
 
 #find all bed, bedpe files, return their md5sums to std out, list all file types
-find ./analysis/structural_variants -name *.bed -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" \;
-find ./analysis/structural_variants -name *.bedpe -xtype f -exec sh -c "cat {} | grep -v ^# |md5sum" \;
-ls ./analysis/structural_variants | sed 's/.*\.//' | sort | uniq -c
+find ./*/analysis/structural_variants -name *.bed -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" \;
+find ./*/analysis/structural_variants -name *.bedpe -xtype f -exec sh -c "cat {} | grep -v ^# |md5sum" \;
+ls ./*/analysis/structural_variants | sed 's/.*\.//' | sort | uniq -c
