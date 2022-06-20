50e1ab81bb797bf02992bb14d08e331e8f615801
Gavin Peng <gpeng@oicr.on.ca>
2022-06-20 10:55:51 -0400

    delete line that will generate repeat results in test command

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,5 +6,4 @@ cd $1
 
 #find all bed, bedpe files, return their md5sums to std out, list all file types
 find -name '*.bed' -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" \;
-find -name '*.bedpe' -xtype f -exec sh -c "cat {} | grep -v ^# |md5sum" \;
 ls .| sed 's/.*\.//' | sort | uniq -c
